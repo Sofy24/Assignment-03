d5af72508300666fa32c4f06af7b8248bf12ca74
Sofy24 <sofia.tosi4@studio.unibo.it>
2023-07-06 12:31:34 +0200

    the two instances are comunicating

--- src/main/java/part2/Users_prova.java
+++ src/main/java/part2/Users_prova.java
@@ -1,74 +1,98 @@
 package part2;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.*;
 import part1.Utils.Pair;
 
+import java.io.IOException;
 import java.util.Random;
+import java.util.concurrent.TimeoutException;
 
 public class Users_prova {
 
   private final static String QUEUE_NAME = "hello";
   private final static String NO_EXCHANGE_USED = "";
 
+  private static final String EXCHANGE_NAME = "peer_exchange";
+
   public static void main(String[] argv) throws Exception {
-    ConnectionFactory factory = new ConnectionFactory();
-    factory.setHost("localhost");
-    Connection connection = factory.newConnection();
-    Channel channel = connection.createChannel();
+    try {
+      ConnectionFactory factory = new ConnectionFactory();
+      factory.setHost("localhost");
+      Connection connection = factory.newConnection();
+      Channel channel = connection.createChannel();
 
-    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+      channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
+      channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+      // Bind the queue to the exchange
+      channel.queueBind(QUEUE_NAME, EXCHANGE_NAME, "");
+
+      // Start consuming messages
+      Consumer consumer = new DefaultConsumer(channel) {
+        @Override
+        public void handleDelivery(String consumerTag, Envelope envelope,
+                                   AMQP.BasicProperties properties, byte[] body) throws IOException {
+          String message = new String(body, "UTF-8");
+          System.out.println("Received message: " + message);
+        }
+      };
+
+      channel.basicConsume(QUEUE_NAME, true, consumer);
+
+      // Publish a message
+      String message2 = "Hello, peers!" + randomColor();
+      channel.basicPublish(EXCHANGE_NAME, "", null, message2.getBytes("UTF-8"));
+      System.out.println("Sent message: " + message2);
 
 
-    var brushManager = new BrushManager();
-    var localBrush = new BrushManager.Brush(0, 0, randomColor());
-    var fooBrush = new BrushManager.Brush(0, 0, randomColor());
-    brushManager.addBrush(localBrush);
-    brushManager.addBrush(fooBrush);
-    PixelGrid grid = new PixelGrid(40,40);
+      var brushManager = new BrushManager();
+      var localBrush = new BrushManager.Brush(0, 0, randomColor());
+      var fooBrush = new BrushManager.Brush(0, 0, randomColor());
+      brushManager.addBrush(localBrush);
+      brushManager.addBrush(fooBrush);
+      PixelGrid grid = new PixelGrid(40, 40);
 
-    Random rand = new Random();
-    for (int i = 0; i < 10; i++) {
-      grid.set(rand.nextInt(40), rand.nextInt(40), randomColor());
+      Random rand = new Random();
+      for (int i = 0; i < 10; i++) {
+        grid.set(rand.nextInt(40), rand.nextInt(40), randomColor());
+      }
+
+      PixelGridView view = new PixelGridView(grid, brushManager, 800, 600);
+
+      view.addMouseMovedListener((x, y) -> {
+        localBrush.updatePosition(x, y);
+        view.refresh();
+        String message = "mouse moved! " + x + " " + y;
+//        channel.basicPublish(NO_EXCHANGE_USED, QUEUE_NAME, null, message.getBytes("UTF-8"));
+//        System.out.println(" [x] Sent '" + message + "'");
+      });
+
+      view.addPixelGridEventListener((x, y) -> {
+        grid.set(x, y, localBrush.getColor());
+        view.refresh();
+        String message = "pixel colored! " + x + " " + y + " " + localBrush.getColor();
+//        channel.basicPublish(NO_EXCHANGE_USED, QUEUE_NAME, null, message.getBytes("UTF-8"));
+//        System.out.println(" [x] Sent '" + message + "'");
+      });
+
+      view.addColorChangedListener(localBrush::setColor);
+
+      view.display();
+
+    } catch (IOException | TimeoutException e) {
+      e.printStackTrace();
     }
-
-    PixelGridView view = new PixelGridView(grid, brushManager, 800, 600);
-
-    view.addMouseMovedListener((x, y) -> {
-      localBrush.updatePosition(x, y);
-      view.refresh();
-      String message = "mouse moved! "+x+" "+y;
-      channel.basicPublish(NO_EXCHANGE_USED, QUEUE_NAME, null, message.getBytes("UTF-8"));
-      System.out.println(" [x] Sent '" + message + "'");
-    });
-
-    view.addPixelGridEventListener((x, y) -> {
-      grid.set(x, y, localBrush.getColor());
-      view.refresh();
-      String message = "pixel colored! "+x+" "+y+" "+localBrush.getColor();
-      channel.basicPublish(NO_EXCHANGE_USED, QUEUE_NAME, null, message.getBytes("UTF-8"));
-      System.out.println(" [x] Sent '" + message + "'");
-    });
-
-    view.addColorChangedListener(localBrush::setColor);
-
-    view.display();
-
-
+  }
 
 
     /*
     channel.close();
     connection.close();
     */
-  }
 
 
   public static int randomColor() {
     Random rand = new Random();
     return rand.nextInt(256 * 256 * 256);
   }
-
 
 
 
